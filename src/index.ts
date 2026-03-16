dcf2ca74e27f40d5ee211de7b4dde7c1eaedc7a2
Natthaphum Phusong <info.hzelnat@gmail.com>
2026-03-16 23:23:40 +0700

    add hook to index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./fetchNui";
 export * from "./hooks/useNuiEvent";
 export * from "./hooks/useKeyBind";
+export * from "./hooks/useVisibility";
+export * from "./hooks/useNuiQuery";
 export * from "./isEnvBrowser";
 export * from "./mock";
